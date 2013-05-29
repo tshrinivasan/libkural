a0c64f52a1989b32d29f549a22c28762e30a9234
Muthiah Annamalai <muthuspost@gmail.com>
2013-05-29 00:17:57 -0400

    setup/install file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='libkural',
+      version='1.0',
+      description='Tamil UTF-8 fast-access database for Thirukkural (G.U.Pope et-al version)',
+      author='Muthiah Annamalai',
+      author_email='',
+      url='https://github.com/arcturusannamalai/libkural',
+      py_modules=['libkural'],
+      )
+
